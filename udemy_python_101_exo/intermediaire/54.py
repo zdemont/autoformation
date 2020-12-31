1cf43e9710b927a75ef7653545672f27c5aa2f68
zdemont <zhangdemont@gmail.com>
2020-12-31 13:14:46 +0100

    exo 54

--- udemy_python_101_exo/intermediaire/54.py
+++ udemy_python_101_exo/intermediaire/54.py
@@ -2,3 +2,17 @@
 n = 8
 symbole = '*'
 
+increment = [symbole*i for i in range(1,n)]
+decrement = increment[::-1]
+increment.append(symbole*n)
+l = increment + decrement
+
+printer = '\n'.join(l)
+print(printer)
+
+
+
+#  solution 
+nombres = list(range(1, n+1)) + list(range(n-1, 0, -1))
+for i in nombres:
+    print(symbole*i)
