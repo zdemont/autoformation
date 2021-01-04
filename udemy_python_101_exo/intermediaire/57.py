473b814d4152047bf85bd66d861877ce8884bc48
zdemont <zhangdemont@gmail.com>
2021-01-04 22:01:16 +0100

    exo 57

--- udemy_python_101_exo/intermediaire/57.py
+++ udemy_python_101_exo/intermediaire/57.py
@@ -1,8 +1,26 @@
 nombre = 52039480394023
 
-longueur = len(str(nombre))
+nombre_ = str(nombre)
+first_couple = len(str(nombre))%3
 
-nb_part_3 = int(longueur / 3)
-nb_part = longueur - nb_part_3
+first_couplenb = [nombre_[0:first_couple]]
+reste = nombre_[first_couple:]
+couple3 = [reste[i*3:(i+1)*3] for i in range(int(len(reste)/3))]
 
-print(longueur, nb_part)
+if '' in first_couplenb:
+    nb_splitted = ','.join(couple3)
+else:
+    nb_splitted = ','.join(first_couplenb + couple3)
+print(nb_splitted)
+
+# solution 
+
+def ajout_separateur(nombre):
+    nombre = str(nombre)[::-1]
+    resultat = ""
+ 
+    for i, chiffre in enumerate(nombre, 1):
+        chiffre_formatte = chiffre + "," if i % 3 == 0 and i != len(nombre) else chiffre
+        resultat += chiffre_formatte
+ 
+    return resultat[::-1]
